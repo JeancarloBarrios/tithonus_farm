345d5fb50e2fe598a61470569dc1cc4e6c4de202
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2021-03-06 23:03:13 -0600

    Added Tithonus_farm_frimware

---
+++ tithonus_farm_firmware/lib/tithonus_farm_firmware.ex
@@ -0,0 +1,18 @@
+defmodule TithonusFarmFirmware do
+  @moduledoc """
+  Documentation for TithonusFarmFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TithonusFarmFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tithonus_farm_firmware/test/tithonus_farm_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule TithonusFarmFirmwareTest do
+  use ExUnit.Case
+  doctest TithonusFarmFirmware
+
+  test "greets the world" do
+    assert TithonusFarmFirmware.hello() == :world
+  end
+end
